0bb05bab97014db2e0fd92c5f12716a08899341c
daizc <daizc311@gmail.com>
2022-03-19 13:26:24 +0800

    tokenResponse

--- pats-server/src/main/java/run/bequick/dreamccc/pats/security/JwtAuthenticationFilter.java
+++ pats-server/src/main/java/run/bequick/dreamccc/pats/security/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package run.bequick.dreamccc.pats.security;
 import cn.hutool.core.date.DateTime;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,6 +24,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -46,19 +49,28 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+
+        int expiresSecond = 600;
+        int refreshSecond = 3600;
+        DateTime dateTime = new DateTime();
+        Calendar accessTokenTime = dateTime.toCalendar();
+        accessTokenTime.add(Calendar.SECOND,expiresSecond);
+        Calendar refreshTokenTime = dateTime.toCalendar();
+        refreshTokenTime.add(Calendar.SECOND,refreshSecond);
+
         // 成功认证刷新Token
         log.info("签发Token");
         User userDetail = (User) authentication.getPrincipal();
         Algorithm algorithm = Algorithm.HMAC256("HMAC256");
         String accessToken = JWT.create()
                 .withSubject(userDetail.getUsername())
-                .withExpiresAt(new DateTime(System.currentTimeMillis() + 10 * 60 * 1000))
+                .withExpiresAt(accessTokenTime.getTime())
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles",userDetail.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
         String refreshToken = JWT.create()
                 .withSubject(userDetail.getUsername())
-                .withExpiresAt(new DateTime(System.currentTimeMillis() + 30 * 60 * 1000))
+                .withExpiresAt(refreshTokenTime.getTime())
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles",userDetail.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
@@ -67,7 +79,14 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         response.setHeader("refresh_token",refreshToken);
 
         response.setContentType(APPLICATION_JSON_VALUE);
-        objectMapper.writeValue(response.getOutputStream(),new TokenResponse(accessToken,refreshToken));
+        TokenResponse tokenResponse = new TokenResponse();
+        tokenResponse.setAccessToken(accessToken);
+        tokenResponse.setRefreshToken(refreshToken);
+        tokenResponse.setAccountId(userDetail.getUsername());
+        tokenResponse.setCreatedAt(dateTime);
+        tokenResponse.setExpiresIn(expiresSecond);
+        tokenResponse.setTokenType("bearer");
+        objectMapper.writeValue(response.getOutputStream(),tokenResponse);
 //        super.successfulAuthentication(request, response, chain, authentication);
     }
 
@@ -75,7 +94,17 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @NoArgsConstructor
     @AllArgsConstructor
     static class TokenResponse{
+        @JsonProperty("access_token")
         private String accessToken;
+        @JsonProperty("created_at")
+        private Date createdAt;
+        @JsonProperty("expires_in")
+        private Integer expiresIn;
+        @JsonProperty("refresh_token")
         private String refreshToken;
+        @JsonProperty("token_type")
+        private String tokenType;
+        @JsonProperty("account_id")
+        private String accountId;
     }
 }
