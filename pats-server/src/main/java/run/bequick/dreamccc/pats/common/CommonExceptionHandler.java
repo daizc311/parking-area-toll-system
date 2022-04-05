0abc037e2df955b1276f0f20ac3388ab5c51bbce
daizc <daizc311@gmail.com>
2022-04-06 03:50:09 +0800

    修复公共异常处理 新增redisTemplate

--- pats-server/src/main/java/run/bequick/dreamccc/pats/common/CommonExceptionHandler.java
+++ pats-server/src/main/java/run/bequick/dreamccc/pats/common/CommonExceptionHandler.java
@@ -4,6 +4,7 @@ import cn.hutool.core.text.StrFormatter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <h3>公共异常处理</h3>
@@ -11,18 +12,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @author Daizc
  */
 @Slf4j
+@ResponseBody
 @ControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public DrResponse<?> businessException(BusinessException exception) {
-        return DrResponse.data(exception.getMessage());
+        return DrResponse.failed(exception.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public DrResponse<?> exception(Exception exception) {
         log.error(StrFormatter.format("发生错误:{}", exception.getMessage()), exception);
-        return DrResponse.data(exception.getMessage());
+        return DrResponse.failed(exception.getMessage());
     }
 
 }
